7502217cda9260a0b1cc0da93fc019f9fcf7c408
Anna Kummer <annamkummer@gmail.com>
2021-09-19 17:25:44 -0600

    Create Comment class

---
+++ Comment.js
@@ -0,0 +1,13 @@
+class Comment {
+  constructor(content) {
+    this.content = content;
+  }
+
+  saveToStorage() {
+// (should only have one job which is to save the instance to storage)
+  }
+
+  deleteFromStorage() {
+
+  }
+}
